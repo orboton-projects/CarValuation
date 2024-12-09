707d15bfc896007da07322e46a4700360022ec96
orboton-projects <orboton@gmail.com>
2024-12-09 10:43:55 +0000

    updated locators

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright
 def browser_setup():
     """Fixture to initialize and clean up Playwright browser."""
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         yield page
